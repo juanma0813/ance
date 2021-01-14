63627e05ad3ec85524eef3a9e03b38859b58cab7
juanma0813 <j.manue.mtz.arelio@gmail.com>
2021-01-14 17:39:07 -0600

    se agregan métodos de conexión
    se agregan métodos de conexión

--- Normanet_web/Normanet_Comunicaciones/Comites/getComites.cs
+++ Normanet_web/Normanet_Comunicaciones/Comites/getComites.cs
@@ -1,27 +1,110 @@
 ﻿using Newtonsoft.Json;
+using Normanet_Entidades.Comites;
 using RestSharp;
 using System;
 using System.Collections.Generic;
 using System.Configuration;
+using System.IO;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml;
 
 namespace Normanet_Comunicaciones.Comites
 {
-    public class getComites
+    public class Comites
     {
-        public Normanet_Entidades.Comites.getComites getComites_Get(String ID_CT,String ID_SC)
+        public static List<Normanet_Entidades.Comites.getComites> getComites_Get(String ID_CT,String ID_SC)
         {
-            string value = System.Configuration.ConfigurationManager.AppSettings["EndpointBase"];
+        string value = System.Configuration.ConfigurationManager.AppSettings["EndpointBase"];
+
             var client = new RestClient(value + "/comitetecnico/CONANCE/" + ID_CT + "/" + ID_SC);
             client.Timeout = -1;
             var request = new RestRequest(Method.GET);
             request.AddHeader("token", "ANC3202!");
             request.AddHeader("Cookie", "ARRAffinity=8f8ac6c076f7a9e2132f2eea1ff0fc61836fde1fef8c5525da0e81359003c9e8; ARRAffinitySameSite=8f8ac6c076f7a9e2132f2eea1ff0fc61836fde1fef8c5525da0e81359003c9e8");
             IRestResponse response = client.Execute(request);
-            Normanet_Entidades.Comites.getComites Response = JsonConvert.DeserializeObject<Normanet_Entidades.Comites.getComites>(response.Content);
+            List<Normanet_Entidades.Comites.getComites> Response = JsonConvert.DeserializeObject<List<Normanet_Entidades.Comites.getComites>>(response.Content);
             return Response;
         }
+        public static List<Normanet_Entidades.Comites.getComitesDescripcion> getComitesDescripcion_Get(GetDeleteComitesDescripcion_Parameter _Parameter)
+        {
+            string value = System.Configuration.ConfigurationManager.AppSettings["EndpointBase"] + "comitetecnico/descripcion";
+
+            var client = new RestClient(value);
+            client.Timeout = -1;
+            var request = new RestRequest(Method.POST);
+            request.AddHeader("token", "ANC3202!");
+            request.AddHeader("Content-Type", "application/json");
+            request.AddHeader("Cookie", "ARRAffinity=8f8ac6c076f7a9e2132f2eea1ff0fc61836fde1fef8c5525da0e81359003c9e8; ARRAffinitySameSite=8f8ac6c076f7a9e2132f2eea1ff0fc61836fde1fef8c5525da0e81359003c9e8");
+            String h = JsonConvert.SerializeObject(_Parameter);
+            request.AddParameter("application/json", JsonConvert.SerializeObject(_Parameter), ParameterType.RequestBody);
+            IRestResponse response = client.Execute(request);
+            List<Normanet_Entidades.Comites.getComitesDescripcion> Response = JsonConvert.DeserializeObject<List<Normanet_Entidades.Comites.getComitesDescripcion>>(response.Content);
+            return Response;
+        }
+        public String addComite_post(addComite_post_put_Parameter _Parameter)
+        {
+            string value = System.Configuration.ConfigurationManager.AppSettings["EndpointBase"];
+
+            var client = new RestClient(value + "comitetecnico");
+            client.Timeout = -1;
+            var request = new RestRequest(Method.POST);
+            request.AddHeader("token", "ANC3202!");
+            request.AddHeader("Content-Type", "application/json");
+            request.AddHeader("Cookie", "ARRAffinity=8f8ac6c076f7a9e2132f2eea1ff0fc61836fde1fef8c5525da0e81359003c9e8; ARRAffinitySameSite=8f8ac6c076f7a9e2132f2eea1ff0fc61836fde1fef8c5525da0e81359003c9e8");
+            request.AddParameter("application/json", JsonConvert.SerializeObject(_Parameter), ParameterType.RequestBody);
+            IRestResponse response = client.Execute(request);
+            return response.StatusCode.ToString();
+        }
+        public String addComite_put(addComite_post_put_Parameter _Parameter)
+        {
+            string value = System.Configuration.ConfigurationManager.AppSettings["EndpointBase"];
+
+            var client = new RestClient(value + "comitetecnico");
+            client.Timeout = -1;
+            var request = new RestRequest(Method.PUT);
+            request.AddHeader("token", "ANC3202!");
+            request.AddHeader("Content-Type", "application/json");
+            request.AddHeader("Cookie", "ARRAffinity=8f8ac6c076f7a9e2132f2eea1ff0fc61836fde1fef8c5525da0e81359003c9e8; ARRAffinitySameSite=8f8ac6c076f7a9e2132f2eea1ff0fc61836fde1fef8c5525da0e81359003c9e8");
+            request.AddParameter("application/json", JsonConvert.SerializeObject(_Parameter), ParameterType.RequestBody);
+            IRestResponse response = client.Execute(request);
+            return response.StatusCode.ToString();
+        }
+        public String addComite_delete(GetDeleteComitesDescripcion_Parameter _Parameter)
+        {
+            string value = System.Configuration.ConfigurationManager.AppSettings["EndpointBase"];
+
+            var client = new RestClient(value + "comitetecnico");
+            client.Timeout = -1;
+            var request = new RestRequest(Method.DELETE);
+            request.AddHeader("token", "ANC3202!");
+            request.AddHeader("Content-Type", "application/json");
+            request.AddHeader("Cookie", "ARRAffinity=8f8ac6c076f7a9e2132f2eea1ff0fc61836fde1fef8c5525da0e81359003c9e8; ARRAffinitySameSite=8f8ac6c076f7a9e2132f2eea1ff0fc61836fde1fef8c5525da0e81359003c9e8");
+            request.AddParameter("application/json", JsonConvert.SerializeObject(_Parameter), ParameterType.RequestBody);
+            IRestResponse response = client.Execute(request);
+            return response.StatusCode.ToString();
+
+        }
+
+
+        private static void CallGetMethod(GetDeleteComitesDescripcion_Parameter _Parameter)
+        {
+            string value = System.Configuration.ConfigurationManager.AppSettings["EndpointBase"] + "comitetecnico/descripcion";
+            HttpClient client = new HttpClient();
+            var json = JsonConvert.SerializeObject(_Parameter);
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri(value),
+                Content = new StringContent(json, Encoding.UTF8, "application/json")
+            };
+            var response = client.SendAsync(request).ConfigureAwait(false);
+            var responseInfo = response.GetAwaiter().GetResult();
+        }
+
+
     }
 }
--- Normanet_web/Normanet_Entidades/Comites/getComites.cs
+++ Normanet_web/Normanet_Entidades/Comites/getComites.cs
@@ -16,4 +16,38 @@ namespace Normanet_Entidades.Comites
         public string Objetivo { get; set; }
         public string Responsable { get; set; }
     }
+    public class getComitesDescripcion
+    {
+        public string ID_Comite { get; set; }
+        public string ID_CT { get; set; }
+        public string ID_SC { get; set; }
+        public string ID_Grupo { get; set; }
+        public string Descripcion { get; set; }
+        public string Objetivo { get; set; }
+        public string Responsable { get; set; }
+        public Boolean Inactivo { get; set; }
+
+    }
+    public class GetDeleteComitesDescripcion_Parameter
+    {
+        public string Bandera { get; set; }
+        public string Comite { get; set; }
+        public string CT { get; set; }
+        public string SC { get; set; }
+        public string GT { get; set; }
+    }
+    public class addComite_post_put_Parameter
+    {
+        public string Bandera { get; set; }
+        public string Comite { get; set; }
+        public string Descripcion { get; set; }
+        public string Responsable { get; set; }
+        public string Inactivo { get; set; }
+        public string Objetivo { get; set; }
+        public string CT { get; set; }
+        public string SC { get; set; }
+        public string GT { get; set; }
+    }
+
+
 }
